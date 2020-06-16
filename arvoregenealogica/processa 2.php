dbddb4acfa3d449542649fd44b49912154a2ddbf
rafap <rafap@rafaelmariani>
2020-06-16 18:07:42 -0300

    comit rafael ok

--- arvoregenealogica/processa 2.php
+++ arvoregenealogica/processa 2.php
@@ -16,8 +16,16 @@ $rua=mysqli_real_escape_string($conn, $_POST['rua']);
 $bairro =mysqli_real_escape_string($conn, $_POST['bairro']);
 $falecido=filter_input(INPUT_POST,'radio_obito', FILTER_SANITIZE_STRING);
 $data_obto = mysqli_real_escape_string($conn, $_POST['data_obto']);
-$sql = "INSERT INTO `cad_parent` (`id_user`, `grau_parent`, `nome_parent`, `sobrenome_parent`, `data_nas`, `cep`, `cidade`, `uf`, `rua`, `bairro`,`falecido`,data_obt) VALUES (NULL, '$grau_parent', '$nome_parent', '$sobrenome_parent', '$data_nas', '$cep', '$cidade', '$uf', '$rua', '$bairro','$falecido','$data_obto')";
+$sql = "INSERT INTO `cad_parent` (`id_user`, `grau_parent`, `nome_parent`, `sobrenome_parent`, `data_nas`, `cep`, `cidade`, `uf`, `rua`, `bairro`,`falecido`,`data_obt`) VALUES (NULL, '$grau_parent', '$nome_parent', '$sobrenome_parent', '$data_nas', '$cep', '$cidade', '$uf', '$rua', '$bairro','$falecido','$data_obto')";
 $slq = mysqli_query($conn, $sql);
 
+if(mysqli_insert_id($conn)){
+	$_SESSION['msg'] = "<p style='color:green;'>Usuário cadastrado com sucesso</p>";
+	header("Location: index.php");
+        mysqli_close($conn);
+}else{
+	$_SESSION['msg'] = "<p style='color:red;'>Usuário não foi cadastrado com sucesso</p>";
+	header("Location: formulario2.php");
+}
 
 ?>
